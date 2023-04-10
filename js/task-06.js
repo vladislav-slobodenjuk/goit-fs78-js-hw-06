0e5859c9414299eb03b981ebb7d92280c71d5744
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-10 22:34:51 +0300

    task-07

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector("#validation-input");
+const validLength = Number(inputEl.getAttribute("data-length"));
+
+function onImputBlur({ currentTarget: { value } }) {
+  const valueLength = value.length;
+
+  if (valueLength === validLength) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+    return;
+  }
+  inputEl.classList.remove("valid");
+  inputEl.classList.add("invalid");
+}
+
+inputEl.addEventListener("blur", onImputBlur);
