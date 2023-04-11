cc6c664a8f343e1928fa644434b1c28f497ca27a
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-11 21:54:22 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btnEl = document.querySelector(".change-color");
+const outputEl = document.querySelector(".color");
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onHandleClick() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+btnEl.addEventListener("click", onHandleClick);
