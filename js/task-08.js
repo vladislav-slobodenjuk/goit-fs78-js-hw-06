05c30c9501482dfe5f7a91314e319f38020fc674
vladislav-slobodenjuk <vladjun@gmail.com>
2023-04-15 00:52:57 +0300

    refactor for good scaling

--- js/task-08.js
+++ js/task-08.js
@@ -3,26 +3,21 @@ const formEl = document.querySelector(".login-form");
 function onFormSubmit(e) {
   e.preventDefault();
 
-  const formData = new FormData(e.currentTarget);
+  const form = e.currentTarget;
+  const formData = new FormData(form);
   const data = {};
+
   formData.forEach((value, key) => (data[key] = value));
+  const values = Object.values(data);
 
-  const { email, password } = e.currentTarget.elements;
-
-  if (email.value.length === 0 || password.value.length === 0) {
-    alert("fill in both fields");
-    return;
+  for (const item of values) {
+    if (item.length === 0) return alert("fill in all fields");
   }
-
-  // Object.values(data).forEach((value) => {
-  //   if (value.length === 0) {
-  //     alert("fill in both fields");
-  //     return;
-  //   }
-  // });
+  // const isEmptyValue = values.some((item) => item.length === 0);
+  // if (isEmptyValue) return alert("fill in all fields");
 
   console.log(data);
-  formEl.reset();
+  form.reset();
 }
 
 formEl.addEventListener("submit", onFormSubmit);
